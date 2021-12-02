2f5c2d353078e34aa378da108306fca049cb5206
User <User@DESKTOP-1KVG84P>
2021-12-02 12:52:35 +0530

    updated jobportal

--- src/main/java/WrapperClass/GmailExtension.java
+++
@@ -1,5 +0,0 @@
-package WrapperClass;
-
-public class GmailExtension {
-
-}
--- src/main/java/com/DB/DBconnect.java
+++ src/main/java/com/DB/DBconnect.java
@@ -2,7 +2,7 @@ package com.DB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+//test
 public class DBconnect {
 
 	private static Connection conn1;
