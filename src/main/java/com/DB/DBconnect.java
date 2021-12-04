be24e12af15b596c413e9f09792aa699a265731f
greeshma <greeshmanaga25@gmail.com>
2021-12-04 21:42:47 +0530

    testing3

--- src/main/java/com/DB/DBconnect.java
+++ src/main/java/com/DB/DBconnect.java
@@ -12,16 +12,14 @@ public class DBconnect {
 		try {
 			if(conn1==null) {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
-				conn1=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","tiger");
+				conn1=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","chitti");
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(conn1);
+		
 		return conn1;
-		//just for test
+		
 	}
-	/*
-	 * public static void main(String[] args) { System.out.println(getconn()); }
-	 */
+	
 }
